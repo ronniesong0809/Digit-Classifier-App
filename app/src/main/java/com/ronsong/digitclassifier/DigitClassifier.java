0dd8732b71948891c7355f53cf5f651474bb174d
Ronnie Song <ronniesong0809@gmail.com>
2020-08-17 20:00:27 -0700

    feat: add digit classifier

---
+++ app/src/main/java/com/ronsong/digitclassifier/DigitClassifier.java
@@ -0,0 +1,136 @@
+package com.ronsong.digitclassifier;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class DigitClassifier {
+
+    private static final int PIXEL_SIZE = 1;
+    private static final int FLOAT_TYPE_SIZE = 4;
+    private static final int OUTPUT_CLASSES_COUNT = 10;
+    private static final String TAG = "DigitClassifier";
+    private static final String MODEL_FILE = "mnist.tflite";
+
+    private Context context;
+    private Interpreter interpreter;
+    private boolean isInitialized = false;
+    private ExecutorService executorService;
+    private int inputImageWidth = 0;
+    private int inputImageHeight = 0;
+    private int modelInputSize = 0;
+
+    DigitClassifier(Context context) {
+        this.context = context;
+        executorService = Executors.newCachedThreadPool();
+    }
+
+    Task<Void> initialize() {
+        return Tasks.call(executorService, () -> {
+            initInterpreter();
+            return null;
+        });
+    }
+
+    private void initInterpreter() throws IOException {
+        AssetManager assetManager = this.context.getAssets();
+        ByteBuffer model = loadModelFile(assetManager);
+        Interpreter.Options options = new Interpreter.Options();
+        options.setUseNNAPI(true);
+        Interpreter interpreter = new Interpreter(model, options);
+        int[] inputShape = interpreter.getInputTensor(0).shape();
+        inputImageWidth = inputShape[1];
+        inputImageHeight = inputShape[2];
+        modelInputSize = FLOAT_TYPE_SIZE * inputImageHeight * inputImageWidth * PIXEL_SIZE;
+        this.interpreter = interpreter;
+        isInitialized = true;
+        Log.d(TAG, "Initialized TF-Lite interpreter");
+    }
+
+    private ByteBuffer loadModelFile(AssetManager assetManager) throws IOException {
+        AssetFileDescriptor fileDescriptor = assetManager.openFd(MODEL_FILE);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    private String classify(Bitmap bitmap){
+        if(!isInitialized)
+            throw new IllegalStateException("TF Lite Interpreter is not initialized yet.");
+        long startTime, elapsedTime;
+        startTime = System.nanoTime();
+        Bitmap resizedImage = Bitmap.createScaledBitmap(bitmap, inputImageWidth, inputImageHeight, true);
+        ByteBuffer byteBuffer = convertBitmapToByteBuffer(resizedImage);
+        elapsedTime = (System.nanoTime() - startTime) / 1_000_000;
+        Log.d(TAG, "Pre-processing time = " + elapsedTime + " ms");
+
+        startTime = System.nanoTime();
+        float[][] result = { new float[OUTPUT_CLASSES_COUNT] };
+        interpreter.run(byteBuffer, result);
+        elapsedTime = (System.nanoTime() - startTime) / 1_000_000;
+        Log.d(TAG, "Inference time = " + elapsedTime + " ms");
+
+        return getOutputString(result[0]);
+    }
+
+    Task<String> classifyAsync(Bitmap bitmap) {
+        return Tasks.call(executorService, () -> classify(bitmap));
+    }
+
+    void close(){
+        Tasks.call(executorService, () -> {
+            interpreter.close();
+            Log.d(TAG, "Closed TF-Lite interpreter.");
+            return null;
+        });
+    }
+
+    private ByteBuffer convertBitmapToByteBuffer(Bitmap bitmap) {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(modelInputSize);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        int[] pixels = new int[inputImageWidth * inputImageHeight];
+        bitmap.getPixels(pixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        for(int pixelValue : pixels) {
+            int
+                    r = (pixelValue >> 16) & 0xFF,
+                    g = (pixelValue >> 8) & 0xFF,
+                    b = pixelValue & 0xFF;
+            float normalizedPixelValue = (r + g + b) / 3.0f / 255.0f;
+            byteBuffer.putFloat(normalizedPixelValue);
+        }
+        return byteBuffer;
+    }
+
+    private String getOutputString(float[] output){
+        int maxIndex = -1;
+        float max = output[0];
+        for(int i = 0; i < output.length; i++){
+            if(output[i] > max) {
+                max = output[i];
+                maxIndex = i;
+            }
+        }
+        return "Prediction Result: " + maxIndex + "\nConfidence: " + max;
+    }
+
+    boolean isInitialized() {
+        return isInitialized;
+    }
+}
--- app/src/main/java/com/ronsong/digitclassifier/MainActivity.java
+++ app/src/main/java/com/ronsong/digitclassifier/MainActivity.java
@@ -1,47 +1,72 @@
 package com.ronsong.digitclassifier;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.divyanshu.draw.widget.DrawView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private DrawView draw_view;
     private Button clear_button;
     private TextView text_view;
+    private DigitClassifier digitClassifier = new DigitClassifier(this);
 
-    @SuppressLint({"ClickableViewAccessibility", "ShowToast", "SetTextI18n"})
+    @SuppressLint({"ClickableViewAccessibility", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         draw_view = findViewById(R.id.draw_view);
-        draw_view.setStrokeWidth(70f);
-        draw_view.setColor(Color.WHITE);
-        draw_view.setBackgroundColor(Color.BLACK);
-        draw_view.setOnTouchListener((view, motionEvent) -> {
-            draw_view.onTouchEvent(motionEvent);
-            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                Toast.makeText(this, MotionEvent.ACTION_UP, Toast.LENGTH_LONG);
-            }
-            return true;
-        });
-
-        text_view = findViewById(R.id.predicted_text);
-
+        if(draw_view != null){
+            draw_view.setStrokeWidth(70f);
+            draw_view.setColor(Color.WHITE);
+            draw_view.setBackgroundColor(Color.BLACK);
+        }
         clear_button = findViewById(R.id.clear_button);
+        text_view = findViewById(R.id.text);
+
         clear_button.setOnClickListener(event -> {
             draw_view.clearCanvas();
             text_view.setText("Please draw a digit");
         });
+
+        draw_view.setOnTouchListener((view, motionEvent) -> {
+            draw_view.onTouchEvent(motionEvent);
+            if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                classifyDrawing();
+            }
+            return true;
+        });
+        digitClassifier
+                .initialize()
+                .addOnFailureListener(e -> Log.e(TAG, "Error to setting up digit classifier.", e));
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void classifyDrawing() {
+        Bitmap bitmap = draw_view.getBitmap();
+        if(digitClassifier.isInitialized()){
+            digitClassifier
+                    .classifyAsync(bitmap)
+                    .addOnSuccessListener(resultText -> text_view.setText(resultText))
+                    .addOnFailureListener(e -> {
+                        text_view.setText("Error");
+                        Log.e(TAG, "Error classifying drawing.", e);
+                    });
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        digitClassifier.close();
+        super.onDestroy();
     }
 }
